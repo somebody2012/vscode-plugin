70107d1b8d7125ccb93a30f81cae977b8593b2fb
zt_diaoxr <diaoxinrui@qq.com>
2020-06-19 18:43:40 +0800

    1

--- out/extension.js
+++ out/extension.js
@@ -19,6 +19,8 @@ const vscode = require("vscode");
 const exec = util.promisify(require('child_process').exec);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+let oldDevIp = "10.16.61.50";
+let newDevIp = "14.0.33.161";
 function findFCClientIndex(nameArr) {
     let rootName = ["FCClient2", "FCClientCommon", "FCClient"];
     let index = -1;
@@ -33,12 +35,20 @@ function findFCClientIndex(nameArr) {
 }
 function buildTrade(buildCWD, buildPath) {
     return new Promise(resolve => {
-        buildPath = buildPath.replace(/\\/g, "\\\\");
+        buildPath = buildPath.replace(/\\/g, "/");
         let sh = `npm run build:trade ${buildPath}`;
+        vscode.window.showInformationMessage(`编译命令：${sh}`);
+        let env = process.env;
+        if (!env.SHEEL_PATH) {
+            vscode.window.showErrorMessage("环境变量SHEEL_PATH为空，比如：D:\\Program Files\\Git\\bin\\bash.exe");
+            resolve(false);
+            return;
+        }
         let options = {
             encoding: "utf-8",
             stdio: [process.stdin, process.stdout, process.stderr],
-            cwd: buildCWD
+            cwd: buildCWD,
+            shell: env.SHEEL_PATH
         };
         let res = exec(sh, options, function (err, stdo, stde) {
             if (err) {
@@ -55,16 +65,24 @@ function buildTrade(buildCWD, buildPath) {
 function uploadFile(localDirPath, filename, remotePath) {
     return new Promise(resolve => {
         let localFilePath = path.resolve(localDirPath, filename);
-        localFilePath = localFilePath.replace(/\\/g, "\\\\\\\\\\\\");
+        localFilePath = localFilePath.replace(/\\/g, "/");
         // let sh = `scp -r -P 22 ${localFilePath} root@10.16.61.50:${remotePath}`//开发
-        let sh = `sftp -P 22 root@10.16.61.50:${remotePath}<<EOF\nput ${localFilePath}\nbye\nEOF`; //开发
+        // let sh = `sftp -P 22 root@${oldDevIp}:${remotePath}<<EOF\nput ${localFilePath}\nbye\nEOF`//old开发
+        let sh = `sftp -P 22 root@${newDevIp}:${remotePath}<<EOF\nput ${localFilePath}\nbye\nEOF`; //new开发
         let sh1 = "sftp -P 22 root@10.16.61.50:/aarm/workspace/www/FCClient/modules/trade/auth/t00040404/t00040404_1<<EOF put e:\\work\\zantong\\stage2xxxxxxxxxxxx\\client\\release\\workspace\\FCClient\\modules\\trade\\auth\\t00040404\\t00040404_1\\a.a EOF";
+        vscode.window.showInformationMessage(`开始上传IP:${sh}`);
         // let sh = `scp -r -P 22 ${localFilePath} root@47.105.154.93:${remotePath}`//阿里云
+        let env = process.env;
+        if (!env.SHEEL_PATH) {
+            vscode.window.showErrorMessage("环境变量SHEEL_PATH为空，比如：D:\\Program Files\\Git\\bin\\bash.exe");
+            resolve(false);
+            return;
+        }
         let options = {
             encoding: "utf-8",
             stdio: [process.stdin, process.stdout, process.stderr],
-            shell: "D:\\Program Files\\Git\\bin\\bash.exe"
-            // shell:"/usr/bin/bash"
+            // shell:"D:\\Program Files\\Git\\bin\\bash.exe"
+            shell: env.SHEEL_PATH
         };
         let res = require('child_process').exec(sh, options, function (err, stdo, stde) {
             if (err) {
@@ -95,6 +113,10 @@ function activate(context) {
         // The commandId parameter must match the command field in package.json
         let disposable = vscode.commands.registerCommand('helloworld.helloWorld', (data) => __awaiter(this, void 0, void 0, function* () {
             // vscode.window.showInformationMessage('Hello World111 from HelloWorld 11!');
+            if (!/FCClient2|FCClientCommon|FCClient/.test(data.fsPath)) {
+                vscode.window.showErrorMessage("工程不对");
+                return;
+            }
             let nameArr = data.fsPath.split(path.sep);
             let FCClientIndex = findFCClientIndex(nameArr);
             //"/root/sshTest/modules/trade/zfjs/t00301032"
@@ -104,12 +126,18 @@ function activate(context) {
             localDirPath = localDirPath.replace(/FCClient2|FCClientCommon/, "FCClient");
             //"modules\trade\zfjs\t00301032\App.vue"
             let buildPath = nameArr.slice(FCClientIndex + 1).join("\\");
-            //build执行命令目录
-            let buildCWD = nameArr.slice(0, FCClientIndex + 1).join(path.sep);
-            vscode.window.showInformationMessage('开始编译');
-            let buildRes = yield buildTrade(buildCWD, buildPath);
-            if (!buildRes)
-                return;
+            if (/\.json$/.test(nameArr[nameArr.length - 1])) {
+                fs.copyFileSync(data.fsPath, localDirPath);
+                vscode.window.showInformationMessage(`拷贝文件${data.fsPath}->${localDirPath} 成功`);
+            }
+            else {
+                //build执行命令目录
+                let buildCWD = nameArr.slice(0, FCClientIndex + 1).join(path.sep);
+                vscode.window.showInformationMessage(`开始编译,编译目录：${buildCWD},编译交易路径：${buildPath}`);
+                let buildRes = yield buildTrade(buildCWD, buildPath);
+                if (!buildRes)
+                    return;
+            }
             let uploadFiles = [];
             try {
                 uploadFiles = fs.readdirSync(localDirPath);
@@ -120,7 +148,6 @@ function activate(context) {
                 vscode.window.showErrorMessage(e.message);
                 return;
             }
-            vscode.window.showInformationMessage("开始上传");
             let ups = uploadFiles.map(function (filename) {
                 return uploadFile(localDirPath, filename, remotePath);
             });
--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,11 @@ import * as vscode from 'vscode';
 const exec = util.promisify(require('child_process').exec);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+let oldDevIp = "10.16.61.50";
+let newDevIp = "14.0.33.161";
+
+
 function findFCClientIndex(nameArr:any){
 	let rootName = ["FCClient2","FCClientCommon","FCClient"];
 	let index = -1;
@@ -25,12 +30,20 @@ function findFCClientIndex(nameArr:any){
 
 function buildTrade(buildCWD:string,buildPath:string){
 	return new Promise(resolve => {
-		buildPath = buildPath.replace(/\\/g,"\\\\")
+		buildPath = buildPath.replace(/\\/g,"/")
 		let sh = `npm run build:trade ${buildPath}`
+		vscode.window.showInformationMessage(`编译命令：${sh}`)
+		let env = process.env
+		if(!env.SHEEL_PATH){
+			vscode.window.showErrorMessage("环境变量SHEEL_PATH为空，比如：D:\\Program Files\\Git\\bin\\bash.exe")
+			resolve(false)
+			return;
+		}
 		let options = {
 			encoding:"utf-8",
 			stdio:[process.stdin,process.stdout,process.stderr],
-			cwd:buildCWD
+			cwd:buildCWD,
+			shell:env.SHEEL_PATH
 		};
 		let res = exec(sh,options,function(err:any,stdo:string,stde:string){
 			if(err){
@@ -48,20 +61,26 @@ function buildTrade(buildCWD:string,buildPath:string){
 function uploadFile(localDirPath:string,filename:string,remotePath:string){
 	return new Promise(resolve => {
 		
+		
 		let localFilePath = path.resolve(localDirPath,filename);
-		localFilePath = localFilePath.replace(/\\/g,"\\\\\\\\\\\\");
+		localFilePath = localFilePath.replace(/\\/g,"/");
 		// let sh = `scp -r -P 22 ${localFilePath} root@10.16.61.50:${remotePath}`//开发
-		let sh = `sftp -P 22 root@10.16.61.50:${remotePath}<<EOF\nput ${localFilePath}\nbye\nEOF`//开发
+		// let sh = `sftp -P 22 root@${oldDevIp}:${remotePath}<<EOF\nput ${localFilePath}\nbye\nEOF`//old开发
+		let sh = `sftp -P 22 root@${newDevIp}:${remotePath}<<EOF\nput ${localFilePath}\nbye\nEOF`//new开发
 		let sh1 = "sftp -P 22 root@10.16.61.50:/aarm/workspace/www/FCClient/modules/trade/auth/t00040404/t00040404_1<<EOF put e:\\work\\zantong\\stage2xxxxxxxxxxxx\\client\\release\\workspace\\FCClient\\modules\\trade\\auth\\t00040404\\t00040404_1\\a.a EOF"
-
+		vscode.window.showInformationMessage(`开始上传IP:${sh}`);
 		// let sh = `scp -r -P 22 ${localFilePath} root@47.105.154.93:${remotePath}`//阿里云
-
-
+		let env = process.env
+		if(!env.SHEEL_PATH){
+			vscode.window.showErrorMessage("环境变量SHEEL_PATH为空，比如：D:\\Program Files\\Git\\bin\\bash.exe")
+			resolve(false)
+			return;
+		}
 		let options = {
 			encoding:"utf-8",
 			stdio:[process.stdin,process.stdout,process.stderr],
-			shell:"D:\\Program Files\\Git\\bin\\bash.exe"
-			// shell:"/usr/bin/bash"
+			// shell:"D:\\Program Files\\Git\\bin\\bash.exe"
+			shell:env.SHEEL_PATH
 		};
 		let res = require('child_process').exec(sh,options,function(err:any,stdo:string,stde:string){
 			if(err){
@@ -97,7 +116,10 @@ export async function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', async(data) => {
 		
 		// vscode.window.showInformationMessage('Hello World111 from HelloWorld 11!');
-		
+		if(!/FCClient2|FCClientCommon|FCClient/.test(data.fsPath)){
+			vscode.window.showErrorMessage("工程不对");
+			return;
+		}
 		let nameArr = data.fsPath.split(path.sep);
 		let FCClientIndex = findFCClientIndex(nameArr);
 		//"/root/sshTest/modules/trade/zfjs/t00301032"
@@ -107,11 +129,17 @@ export async function activate(context: vscode.ExtensionContext) {
 		localDirPath = localDirPath.replace(/FCClient2|FCClientCommon/,"FCClient");
 		//"modules\trade\zfjs\t00301032\App.vue"
 		let buildPath = nameArr.slice(FCClientIndex+1).join("\\");
-		//build执行命令目录
-		let buildCWD = nameArr.slice(0,FCClientIndex+1).join(path.sep);
-		vscode.window.showInformationMessage('开始编译');
-		let buildRes = await buildTrade(buildCWD,buildPath);
-		if(!buildRes) return;
+		if(/\.json$/.test(nameArr[nameArr.length-1])){
+			fs.copyFileSync(data.fsPath,localDirPath);
+			vscode.window.showInformationMessage(`拷贝文件${data.fsPath}->${localDirPath} 成功`)
+		}else{
+			//build执行命令目录
+			let buildCWD = nameArr.slice(0,FCClientIndex+1).join(path.sep);
+			vscode.window.showInformationMessage(`开始编译,编译目录：${buildCWD},编译交易路径：${buildPath}`);
+			let buildRes = await buildTrade(buildCWD,buildPath);
+			if(!buildRes) return;
+		}
+		
 		let uploadFiles = [];
 		try{
 			uploadFiles = fs.readdirSync(localDirPath);
@@ -121,7 +149,7 @@ export async function activate(context: vscode.ExtensionContext) {
 			vscode.window.showErrorMessage(e.message);
 			return;
 		}
-		vscode.window.showInformationMessage("开始上传");
+		
 		let ups = uploadFiles.map(function(filename:string) {
 			return uploadFile(localDirPath,filename,remotePath)
 		})
